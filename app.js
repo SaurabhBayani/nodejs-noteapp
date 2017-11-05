be290ee2f0fac7aff3211ed88ad66edf834c14f6
Saurabh Bayani <saurabh.bayani@gmail.com>
2017-11-06 01:52:55 +0530

    Added Duoplication logic + Code restructure

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ const command = process.argv[2].toUpperCase();
 
 switch(command){
     case 'ADD':
-        notesModule.addNote(args.title, args.body);
+        var note = notesModule.addNote(args.title, args.body);
+        if(note){
+            // note is created
+            console.log('New Note Created');
+            console.log('------------------');
+            console.log(`Title: ${note.title}`);
+            console.log(`Body: ${note.body}`);
+        } else {
+            // not is already there
+            console.log('Note already present')
+        }
         break;
     case 'DELETE':
         notesModule.deleteNote('title');
--- notes.js
+++ notes.js
@@ -1,20 +1,38 @@
 const fsModule = require('fs');
 
-var addNote = (title, body) => {
-    let notes = [];
-    let note = {
-        title,
-        body
+// Function to fetch notes
+var fetchNotes = () => {
+    try {
+        let notesString = fsModule.readFileSync('./notes.json');
+        return JSON.parse(notesString);
+    } catch(e) {
+        return [];
     }
-    let notesString = fsModule.readFileSync('./notes.json');
-    notes = JSON.parse(notesString);
-    notes.push(note);
-    notesString = JSON.stringify(notes);
+}
+
+// Function to save notes
+var saveNotes = (notes) => {
     fsModule.writeFileSync('./notes.json', notesString, function(err){
         if(err) {
             console.log(err);
         }
     });
+}
+
+var addNote = (title, body) => {
+    let notes = fetchNotes();
+    let note = {
+        title,
+        body
+    }
+    var duplicateNotes =  notes.filter((note) => note.title === title);
+    if(duplicateNotes.length === 0) {
+        notes.push(note);
+        notesString = JSON.stringify(notes);
+        saveNotes(notesString);
+        return note;
+    }
+
 }
 
 var deleteNote = (title) => {
